e2889efbeaa9c8a1b0872c6f7cff04f524e89b19
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2022-12-06 09:01:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+console.log(10)
+console.log('hello')
+//console.log('world)
+//creating a variable
+let num  = 10
+let message = 'Hello'
+console.log(num)
+console.log(message)
+
+let n;
+n=20
+console.log(n)
+
+let a=90, b='great'
+console.log(a,b)
+//automaticaly add a semicolon
+    
+let name='aayush'; console.log(name);
+
+//interaction
+//alert('Hello world')
+
+let askFromUser = prompt('Test')
+console.log(askFromUser)
+
+let isProgrammer = confirm("Are you a programmer")
+console.log(isProgrammer)
+
+
+//single line comment
+
+/*let
+a=
+10*/
+
+//common datatypes
+
+//string => text
+//Number => int as well as float
+//Arrays => orederd pair of collection[10,20]
+//Objects => key value pair{name: 'john}
+//boolean => true and false
+// {} => a block of code
+
+if(true){
+    alert("hello")
+}
+alert('New')
+
+let year = 2022
+if (year==2022){
+    alert("You are so smart")
+
+} else {
+    alert('You are dump')
+}
+
+let age= prompt("how old are you")
+if (age<18){
+    alert('access not allowed')
+}else if (age<20){
+    alert('you have limited access')
+}else{
+    alert('here is you pass to theatre')
+}
