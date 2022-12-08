0a63af420f1168e7eb2dbbceef6ba1f5747638c8
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2022-12-08 08:59:46 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,58 @@
+// while(condition){
+
+// }
+//Print number's between 1 to 5
+// let i = 1
+// while(i<6){
+//     console.log(i)
+//     i++
+// }
+
+// let a = 5
+// while(a<6){
+//     console.log(a)  ####error####
+//     a--
+// }
+
+let i = 0
+do{
+   console.log(i)
+   i++
+}while(i<5) //print code and then check the condition
+
+do{
+    console.log("Hello world")
+
+}while(false)
+
+//for loop
+// for(begin;condition;step){
+
+//}
+for(let i = 0;i<5;i++){
+    console.log(i)
+}
+//skiping the part
+let n = 0
+for(;i<10;i++){
+    console.log(i++)
+}
+// let sum = 0
+// while(true){
+//     let value = prompt("Enter the number:",'')
+//     sum = sum + value
+// }
+// alert('Sum:' +sum)
+
+// let b=0
+// while(i<10){         error
+//     if(i==5)break
+//     console.log(b)
+// }
+
+for (let c=0;c<10;c++){
+    if (c==5 ||  c==7)continue
+    console.log(c) 
+}
+
+
